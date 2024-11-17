d670ddce974b67223955d9e6decd827d352fbe90
matiuskm <matius.km@gmail.com>
2024-11-17 21:13:36 +0700

    refactor auth service test unit

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -10,10 +10,19 @@ describe("AuthService", () => {
     let fakeUsersService: Partial<UsersService>
 
     beforeEach(async () => {
+        const users: User[] = []
+
         // create a fake copy of users service
         fakeUsersService = {
-            find: () => Promise.resolve([]),
-            create: (email: string, password: string) => Promise.resolve({ id: 1, email, password } as User)
+            find: (email: string) => {
+                const filteredUsers = users.filter(user => user.email === email)
+                return Promise.resolve(filteredUsers)
+            },
+            create: (email: string, password: string) => {
+                const user = { id: Math.floor(Math.random() * 999999), email, password } as User
+                users.push(user)
+                return Promise.resolve(user)
+            }
         }
 
         const module = await Test.createTestingModule({
@@ -39,9 +48,28 @@ describe("AuthService", () => {
     })
 
     it('throws an error if signup is called with used email', async () => {
-        fakeUsersService.find = () => Promise.resolve([{ id: 1, email: 'Qm2dE@example.com', password: 'asdf' } as User])
+        await service.signup('Qm2dE@example.com', 'asdf')
         await expect(service.signup('Qm2dE@example.com', 'asdf')).rejects.toThrow(
             BadRequestException,
         )
+    })
+
+    it('throws if signin is called with unused email', async () => {
+        await expect(service.signin('Qm2dE@example.com', 'asdf')).rejects.toThrow(
+            BadRequestException,
+        )
+    })
+
+    it('throws if signin is called with wrong password', async () => {
+        await service.signup('Qm2dE@example.com', 'asdf')
+        await expect(service.signin('Qm2dE@example.com', '2312')).rejects.toThrow(
+            BadRequestException,
+        )
+    })
+
+    it('returns user if correct password is provided', async () => {
+        await service.signup('Qm2dE@example.com', 'asdf')        
+        const user = await service.signin('Qm2dE@example.com', 'asdf')
+        expect(user).toBeDefined()
     })
 })
