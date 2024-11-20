4e3757c6e7e6d1cefa4bb001b304d2a861ccd385
matiuskm <matius.km@gmail.com>
2024-11-20 23:15:02 +0700

    separating databases between each environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,34 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
 import { APP_PIPE } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 const cookieSession = require('cookie-session')
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'sqlite',
-    database: 'db.sqlite',
-    entities: [User, Report],
-    synchronize: true // do not set to true on production
-  }),UsersModule, ReportsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database: 'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true // do not set to true on production
+    // }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          synchronize: true,
+          entities: [User, Report]
+        }
+      }
+    }),
+    UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService, {
     provide: APP_PIPE,
