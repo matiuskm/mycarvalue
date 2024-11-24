0f77b86eae5d911ca96245ee6669010ee8443ffc
matiuskm <matius.km@gmail.com>
2024-11-24 18:06:08 +0700

    create an endpoint for creating reports

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DeepPartial, Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+
+    constructor(@InjectRepository(Report) private readonly repo: Repository<Report>) { }
+
+    create(reportDto: CreateReportDto) {
+        const report = this.repo.create(reportDto as DeepPartial<Report>)
+        return this.repo.save(report)
+    }
+}
+ 
